37ecdf6086316714fe2083631bbeac928186162b
kevin <kl023011@163.com>
2021-06-30 14:53:52 +0800

    .

--- api-test.sh
+++ api-test.sh
@@ -1,4 +1,4 @@
 export LD_LIBRARY_PATH=./engine/libs
 modelpath=./models
 filepath=./file.txt
-./t2s-engine-api --model=$modelpath --file-name=$modelpath
+./t2s-engine-api --model=$modelpath --file-name=$filepath
